3fee51bf8d4fe9f46e307a1f3a9e0a4ba77a3e67
rafal-nowak <contact@rafal-nowak.com>
2023-12-16 09:30:50 +0100

    introduced new version of update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,26 +6,43 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Change to the directory where the docker-compose.yml file is located
 cd "$SCRIPT_DIR"
 
-# Name of your Docker Compose service
-SERVICE_NAME="myapp"
+# Define the Docker image name and repository
+IMAGE_NAME="raspberrypi-blinking-led"
+REPOSITORY="rafalnowak444"
 
-# Use docker-compose to pull the latest images and capture the output
-PULL_OUTPUT=$(docker-compose pull 2>&1)
+# Get the latest version (tag) from the docker-compose.yml file
+DOCKER_COMPOSE_TAG=$(yq '.services.myapp.image' docker-compose.yml | awk -F ':' '{print $2}' | sed 's/[^a-zA-Z0-9_-]//g')
 
-# Check if the output contains the string "Image is up to date" or "Pull complete"
-if [[ $PULL_OUTPUT == *"Image is up to date"* || $PULL_OUTPUT == *"Pull complete"* ]]; then
-    # No changes in the images, no need to stop and recreate the container
-    echo "No changes in images. Container is up to date."
-else
-    # There are changes in the images, stop and recreate the container
-    echo "Changes detected in images. Stopping and recreating the container..."
+# Use the Docker Hub API to get the tags for the image
+TAGS=$(curl -s "https://registry.hub.docker.com/v2/repositories/${REPOSITORY}/${IMAGE_NAME}/tags/" | jq -r '.results[].name')
 
-    # Check if the container is running
-    if docker-compose ps | grep -q "$SERVICE_NAME"; then
-        # Stop the container if it's running
-        docker-compose down
+# Find the newest version (tag)
+NEWEST_VERSION=""
+for TAG in $TAGS; do
+    if [[ "$TAG" > "$NEWEST_VERSION" ]]; then
+        NEWEST_VERSION="$TAG"
     fi
+done
 
+echo "Newest version of ${REPOSITORY}/${IMAGE_NAME} is: ${NEWEST_VERSION}"
+echo "Current version of ${REPOSITORY}/${IMAGE_NAME} is: ${DOCKER_COMPOSE_TAG}"
+
+# Check if the Docker Compose tag is different from the newest version
+if [ "$DOCKER_COMPOSE_TAG" != "$NEWEST_VERSION" ]; then
+    echo "Updating docker-compose.yml with the latest version..."
+
+    # Update the docker-compose.yml file with the latest tag
+    yq -i -y ".services.myapp.image = \"$REPOSITORY/$IMAGE_NAME:$NEWEST_VERSION\"" docker-compose.yml
+
+    # Recreate the container with the latest image
+    docker-compose down
+    #docker-compose up -d
+else
+    echo "Docker Compose is already using the latest version."
 fi
+
+# Clean up Docker resources (prune images and remove dangling volumes)
+#docker system prune -a -f
+#docker volume ls -qf dangling=true | xargs -r docker volume rm
 
 docker-compose up -d
